c98e395ebfda0b4c3a55d2e05299e268938beeb9
gavrinjo <gavrinjo@gmx.com>
2020-02-21 14:10:18 +0100

    added day_10 script (WIP)

---
+++ 30_days_of_code/10_binary_numbers.py
@@ -0,0 +1,14 @@
+# TODO: Day 10: Binary Numbers
+
+import math
+import os
+import random
+import re
+import sys
+
+# HackerRank STDIN parameters test case 0
+# n = int(5)
+
+if __name__ == '__main__':
+    n = int(input())
+    print(bin(n))
