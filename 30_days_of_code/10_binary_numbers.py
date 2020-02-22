325c705ddaeb62a49dac55d62e2a333e6004b65f
gavrinjo <gavrinjo@gmx.com>
2020-02-22 03:53:36 +0100

    exercise solved - Day 10: Binary Numbers number of consecutive 1's in n's binary representation

--- 30_days_of_code/10_binary_numbers.py
+++ 30_days_of_code/10_binary_numbers.py
@@ -1,14 +1,20 @@
 # TODO: Day 10: Binary Numbers
 
+# TODO: Task - Given a base -10 integer ,n, convert it to binary (base -2). Then find and print the base -10 integer
+#  denoting the maximum number of consecutive 1's in n's binary representation.
+"""
 import math
 import os
 import random
 import re
 import sys
-
+"""
 # HackerRank STDIN parameters test case 0
 # n = int(5)
 
 if __name__ == '__main__':
     n = int(input())
-    print(bin(n))
+    lis = "".join(bin(n)[2::]).split("0")
+    lis = sorted(lis)
+    lis.reverse()
+    print(eval("+".join(lis[0])))
