ba0055cdae99c7ce9ae1f06d30a55a0918865fe9
gavrinjo <gavrinjo@gmx.com>
2020-02-22 03:59:36 +0100

    indentation fix

--- 30_days_of_code/03_conditional.py
+++ 30_days_of_code/03_conditional.py
@@ -12,7 +12,7 @@ import sys
 if __name__ == '__main__':
     n = int(input())
 
-if n % 2 == 0 and (n in range(2, 6) or n > 20):
-    print("Not Weird")
-else:
-    print("Weird")
+    if n % 2 == 0 and (n in range(2, 6) or n > 20):
+        print("Not Weird")
+    else:
+        print("Weird")
