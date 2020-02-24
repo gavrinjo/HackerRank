a5b74ec049210de1f5d4ae41074df98861e96e31
gavrinjo <gavrinjo@gmx.com>
2020-02-24 08:02:18 +0100

    Day 12: Inheritance - exercise completed

--- 30_days_of_code/12_inheritance.py
+++ 30_days_of_code/12_inheritance.py
@@ -38,27 +38,45 @@ class Person:
 class Student(Person):
 
 
-# Class Constructor
-#
-#   Parameters:
-#   firstName - A string denoting the Person's first name.
-#   lastName - A string denoting the Person's last name.
-#   id - An integer denoting the Person's ID number.
-#   scores - An array of integers denoting the Person's test scores.
-#
-# Write your constructor here
+    # Class Constructor
+    #
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+    def __init__(self, firstName, lastName, idNumber, scores):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.idNumber = idNumber
+        self.scores = scores
 
-
-#   Function Name: calculate
-#   Return: A character denoting the grade.
-#
-# Write your function here
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+    def calculate(self):
+        avr = int(sum(self.scores)/len(self.scores))
+        if avr in range (90, 101):
+            return "O"
+        elif avr in range(80, 90):
+            return "E"
+        elif avr in range(70, 80):
+            return "A"
+        elif avr in range(55, 70):
+            return "P"
+        elif avr in range(40, 55):
+            return "D"
+        else:
+            return "T"
 
 line = input().split()
 firstName = line[0]
 lastName = line[1]
 idNum = line[2]
-numScores = int(input())  # not needed for Python
+# numScores = int(input())  # not needed for Python
 scores = list(map(int, input().split()))
 s = Student(firstName, lastName, idNum, scores)
 s.printPerson()
