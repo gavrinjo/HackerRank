a32806815136af63079169547e7c53eb69926b9a
gavrinjo <gavrinjo@gmx.com>
2020-03-04 15:21:37 +0100

    added project euler problem_0003.py - wip

---
+++ Practice/project_euler/problem_0003.py
@@ -0,0 +1,44 @@
+"""
+TODO: Largest prime factor
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+"""
+
+#n = int(13195)
+from math import sqrt
+
+n = int(input("enter number of prime numbers you want to print out?\n"))
+
+
+def isPrime(n):
+    if n == 2:
+        return True
+    elif n < 1 or not n % 2:
+        return False
+    for i in range(3, int(sqrt(n) + 1), 2):
+        if not n % i:
+            return False
+    else:
+        return True
+
+
+def primesList(num):
+    primes = [2, ]
+    noPrimes = 1
+    testNum = 3
+
+    while noPrimes < num:
+        if isPrime(testNum):
+            primes.append(testNum)
+            noPrimes += 1
+        testNum += 1
+    return primes
+
+
+def fList(n):
+    pass
+
+
+
+print(list(primesList(n)))
