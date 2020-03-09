55c46f77f765d55dcd5b0e813bf1836738aeefa1
gavrinjo <gavrinjo@gmx.com>
2020-03-09 15:14:01 +0100

    added problem_0004.py - WIP

---
+++ Practice/project_euler/problem_0004.py
@@ -0,0 +1,12 @@
+
+
+def calculate(n):
+    s = []
+    for i in range(d):
+        s.append("9")
+    return int("".join(s))
+
+
+if __name__ == '__main__':
+    d = int(input())
+    print(calculate(d))
