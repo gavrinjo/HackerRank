a3e65374b3af98d2427bf34e43050ecc9c67a909
gavrinjo <gavrinjo@gmx.com>
2020-03-10 12:30:14 +0100

    20_sorting.py - need more about this topic

--- 30_days_of_code/20_sorting.py
+++ 30_days_of_code/20_sorting.py
@@ -1,9 +1,11 @@
 # Day 20: Sorting
 
+import sys
+
 # arr = [1, 2, 3]
 
-num = int(input().strip())
-lst = list(map(int, input().strip().split(' ')))
+# num = int(input().strip())
+# lst = list(map(int, input().strip().split(' ')))
 
 
 def min_swaps(a):
@@ -51,4 +53,25 @@ def min_swaps(a):
     return ans
 
 
-print(min_swaps(lst))
+# print(min_swaps(lst))
+
+n = int(input().strip())
+a = list(map(int, input().strip().split(' ')))
+# Write Your Code Here
+
+swaps = 0
+is_sorted = False
+
+while not is_sorted:
+    is_sorted = True
+    i = 0
+    for i in range(0, len(a)):
+        if i < len(a) - 1:
+            if a[i] > a[i+1]:
+                a[i], a[i+1] = a[i+1], a[i]
+                is_sorted = False
+                swaps += 1
+
+print('Array is sorted in {} swaps.'.format(swaps))
+print('First Element: {}'.format(a[0]))
+print('Last Element: {}'.format(a[len(a)-1]))
